488aa315927a1258e59cc5d998c1eec91748f641
Rija Lalaina <rijalaraz@gmail.com>
2024-08-08 22:26:14 +0300

    29 - Signing in users

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -40,3 +40,11 @@ export async function registerUser(data:RegisterSchema): Promise<ActionResult<Us
     }
 
 }
+
+export async function getUserByEmail(email:string) {
+    return prisma.user.findUnique({where: {email}});
+}
+
+export async function getUserById(id:string) {
+    return prisma.user.findUnique({where: {id}});
+}
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,26 @@
-import GitHub from "next-auth/providers/github"
 import type { NextAuthConfig } from "next-auth"
+import credentials from "next-auth/providers/credentials"
+import { loginSchema } from "./lib/schemas/loginSchema"
+import { getUserByEmail } from "./app/actions/authActions";
+import { compare } from "bcryptjs";
  
-export default { providers: [GitHub] } satisfies NextAuthConfig
+export default {
+    providers: [credentials({
+        name: 'credentials',
+        async authorize(credentials) {
+            const validated = loginSchema.safeParse(credentials);
+
+            if(validated.success) {
+                const {email, password} = validated.data;
+
+                const user = await getUserByEmail(email);
+
+                if(!user || !(await compare(password, user.passwordHash))) return null;
+
+                return user;
+            }
+
+            return null;
+        },
+    })]
+} satisfies NextAuthConfig
